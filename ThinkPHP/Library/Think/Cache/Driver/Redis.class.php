3eb00265254b17b3e7b3f43974dbf53ee9fac99b
xujiaming <2368272774@qq.com>
2018-02-24 01:18:29 +0800

    修改缓存类

--- ThinkPHP/Library/Think/Cache/Driver/Redis.class.php
+++ ThinkPHP/Library/Think/Cache/Driver/Redis.class.php
@@ -2,46 +2,51 @@
 // +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
 // +----------------------------------------------------------------------
-// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// | Copyright (c) 2006-2013 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 namespace Think\Cache\Driver;
 use Think\Cache;
 defined('THINK_PATH') or exit();
 
 /**
- * Redis缓存驱动 
- * 要求安装phpredis扩展：https://github.com/nicolasff/phpredis
+ * Redis缓存驱动
  */
 class Redis extends Cache {
-	 /**
-	 * 架构函数
+    /**
+     * 架构函数
      * @param array $options 缓存参数
      * @access public
      */
     public function __construct($options=array()) {
         if ( !extension_loaded('redis') ) {
-            E(L('_NOT_SUPPORT_').':redis');
+            E(L('_NOT_SUPPERT_').':redis');
         }
-        $options = array_merge(array (
-            'host'          => C('REDIS_HOST') ? : '127.0.0.1',
-            'port'          => C('REDIS_PORT') ? : 6379,
-            'timeout'       => C('DATA_CACHE_TIMEOUT') ? : false,
-            'persistent'    => false,
-        ),$options);
-
+        if(empty($options)) {
+            $options = array (
+                'host'          => C('REDIS_HOST') ? C('REDIS_HOST') : '127.0.0.1',
+                'port'          => C('REDIS_PORT') ? C('REDIS_PORT') : 6379,
+                'timeout'       => C('REDIS_TIMEOUT') ? C('REDIS_TIMEOUT') : false,
+                'auth'      => C('REDIS_AUTH') ? C('REDIS_AUTH'):null,//auth认证
+                'persistent'    => C('REDIS_PERSISTENT') ? C('REDIS_PERSISTENT') : false,
+            );
+        }
         $this->options =  $options;
         $this->options['expire'] =  isset($options['expire'])?  $options['expire']  :   C('DATA_CACHE_TIME');
-        $this->options['prefix'] =  isset($options['prefix'])?  $options['prefix']  :   C('DATA_CACHE_PREFIX');        
-        $this->options['length'] =  isset($options['length'])?  $options['length']  :   0;        
+        $this->options['prefix'] =  isset($options['prefix'])?  $options['prefix']  :   C('DATA_CACHE_PREFIX');
+        $this->options['length'] =  isset($options['length'])?  $options['length']  :   0;
         $func = $options['persistent'] ? 'pconnect' : 'connect';
         $this->handler  = new \Redis;
         $options['timeout'] === false ?
             $this->handler->$func($options['host'], $options['port']) :
             $this->handler->$func($options['host'], $options['port'], $options['timeout']);
+
+        //Auth参数
+        if($this->options['auth']!=null)
+        {
+            $this->handler->auth($this->options['auth']);
+        }
     }
 
     /**
@@ -54,7 +59,7 @@ class Redis extends Cache {
         N('cache_read',1);
         $value = $this->handler->get($this->options['prefix'].$name);
         $jsonData  = json_decode( $value, true );
-        return ($jsonData === NULL) ? $value : $jsonData;	//检测是否为JSON数据 true 返回JSON解析数组, false返回源数据
+        return ($jsonData === NULL) ? $value : $jsonData;   //检测是否为JSON数据 true 返回JSON解析数组, false返回源数据
     }
 
     /**
@@ -73,7 +78,7 @@ class Redis extends Cache {
         $name   =   $this->options['prefix'].$name;
         //对数组/对象数据进行缓存处理，保证数据完整性
         $value  =  (is_object($value) || is_array($value)) ? json_encode($value) : $value;
-        if(is_int($expire) && $expire) {
+        if(is_int($expire)) {
             $result = $this->handler->setex($name, $expire, $value);
         }else{
             $result = $this->handler->set($name, $value);
