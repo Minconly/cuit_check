89e2afe0f37c77f14e5b51bf1dfdb5349bc5d17e
xujiaming <2368272774@qq.com>
2018-03-13 20:02:22 +0800

    解除作弊

--- Application/Worker/Controller/ExamManageController.class.php
+++ Application/Worker/Controller/ExamManageController.class.php
@@ -7,40 +7,55 @@
  */
 namespace Worker\Controller;
 use Think\Controller;
-use Common\Controller\StudentBaseController as stuControlller;
+//use Common\Controller\StudentBaseController as stuControlller;
+use Predis\Autoloader;
+use Predis\Client;
 use \GatewayWorker\Lib\Gateway;
 
 
 class ExamManageController extends Controller{
+    protected  $redis;
 
     //设为作弊
     public function setCheat(){
         $course_id = I("post.cid");
         $paper_id = I("post.pid");
+        $name = I("post.name");
         $client_id = I("post.client_id","");
+        $account = I("post.account","");
         $tag = 'TestInfo:'.$course_id.':'.$paper_id;
-        $student = $tag.":".session("stu_account");
+        $student = $tag.":".$account;
         $student = $student.":studentTestingInfo";
 
-        $studentController = new stuControlller();
-        $redis = $studentController->getRedis();
+        $redis = $this->getRedis();
 
         $arr = [];
+
+        $isok = false;
         //设置redis缓存中的数据
-        if($redis->hset($student,"is_cheat",1) == 0 ){
-            $arr["status"] = 1;
-            $arr["msg"] = "标记成功！";
-            if($client_id !== ""){
-                $message = array(
-                    'type'=>'cheat',
-                    'message'=>'您被标记为作弊！',
-                    'time'=>date('Y-m-d H:i:s'),
-                );
-                if(Gateway::sendToClient($client_id,json_encode($message))){
-                    $arr["msg"] = "标记作弊成功，并且将其踢下线";
-                }else{
-                    $arr["msg"] = "标记作弊成功，但踢下线失败";
+        if($redis->hexists($student,"is_cheat") && $redis->hset($student,"is_cheat",1) == 0 ){
+            if(!M('cheat')->where(array("account"=>$account,"cid"=>$course_id,"pid"=>$paper_id))->find()){
+                $isok = M('cheat')->data(array("account"=>$account,"cid"=>$course_id,"pid"=>$paper_id,"cheat"=>1,"name"=>$name))->add();
+            }else{
+                $isok = M('cheat')->data(array("cheat"=>1))->where(array("account"=>$account,"cid"=>$course_id,"pid"=>$paper_id))->save();
+            }
+            if($isok){
+                $arr["status"] = 1;
+                $arr["msg"] = "标记成功！";
+                if($client_id !== ""){
+                    $message = array(
+                        'type'=>'cheat',
+                        'message'=>'您被标记为作弊！',
+                        'time'=>date('Y-m-d H:i:s'),
+                    );
+                    if(Gateway::sendToClient($client_id,json_encode($message))){
+                        $arr["msg"] = "标记作弊成功，并且将其踢下线";
+                    }else{
+                        $arr["msg"] = "标记作弊成功，但踢下线失败";
+                    }
                 }
+            }else{
+                $redis->hset($student,"is_cheat",0);
             }
 
         }else{
@@ -49,6 +64,51 @@ class ExamManageController extends Controller{
         }
 
         $this->ajaxReturn($arr,"JSON");
+    }
+
+
+    //取消作弊
+    public function unSetCheat(){
+        $course_id = I("post.cid");
+        $paper_id = I("post.pid");
+        $account = I("post.account","");
+        $tag = 'TestInfo:'.$course_id.':'.$paper_id;
+        $student = $tag.":".$account;
+        $student = $student.":studentTestingInfo";
+
+        $redis = $this->getRedis();
+
+        $arr = [];
+        $isok = false;
+        //设置redis缓存中的数据
+        if($redis->hexists($student,"is_cheat") && $redis->hset($student,"is_cheat",0) == 0 ){
+            if(!M('cheat')->where(array("account"=>$account,"cid"=>$course_id,"pid"=>$paper_id))->find()){
+                $arr["status"] = 0;
+                $arr["msg"] = "解除失败，未标记过！";
+            }else{
+                $isok = M('cheat')->data(array("cheat"=>0))->where(array("account"=>$account,"cid"=>$course_id,"pid"=>$paper_id))->save();
+            }
+            if($isok){
+                $arr["status"] = 1;
+                $arr["msg"] = "解除成功！";
+            }
+
+        }else{
+            $arr["status"] = 0;
+            $arr["msg"] = "解除失败！";
+        }
+
+        $this->ajaxReturn($arr,"JSON");
+    }
+
+    //获得redis实例
+    public function getRedis(){
+        //自动加载predis
+        Autoloader::register();
+        if($this->redis == null) {
+            $this->redis = new Client(C('PREDIS_OPTIONS_STUDENT'));
+        }
+        return $this->redis;
     }
 
 }
