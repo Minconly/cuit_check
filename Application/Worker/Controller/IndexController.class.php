3be456db2b5f2ae0355f9eae328fd2d062c21780
taolei <lei.tao@yingxiong.com>
2018-03-21 17:45:08 +0800

    xx

--- Application/Worker/Controller/IndexController.class.php
+++ Application/Worker/Controller/IndexController.class.php
@@ -24,20 +24,20 @@ class IndexController  extends Controller{
         $gateway = new Gateway("Websocket://0.0.0.0:7272");
 
         // 设置名称，方便status时查看
-                $gateway->name = 'ChatGateway';
+        $gateway->name = 'ChatGateway';
         // 设置进程数，gateway进程数建议与cpu核数相同
-                $gateway->count = 4;
+        $gateway->count = 4;
         // 分布式部署时E请设置成内网ip（非127.0.0.1）
-                $gateway->lanIp = '127.0.0.1';
+        $gateway->lanIp = '127.0.0.1';
         // 内部通讯起始端口。假如$gateway->count=4，起始端口为2300
         // 则一般会使用2300 2301 2302 2303 4个端口作为内部通讯端口
-                $gateway->startPort = 2300;
+        $gateway->startPort = 2300;
         // 心跳间隔
-                $gateway->pingInterval = 10;
+        $gateway->pingInterval = 10;
         // 心跳数据
-                $gateway->pingData = '{"type":"ping"}';
+        $gateway->pingData = '{"type":"ping"}';
         // 服务注册地址
-                $gateway->registerAddress = '127.0.0.1:1236';
+        $gateway->registerAddress = '127.0.0.1:1236';
 
         // bussinessWorker 进程
         $worker = new BusinessWorker();
